f10681dc2d687a84ea89fb9520b80e5c73204836
deivisonmarteleto <42618693+deivisonmarteleto@users.noreply.github.com>
2020-03-09 14:08:40 -0300

    Update notify.sh

--- container/fastnetmon/bin/notify.sh
+++ container/fastnetmon/bin/notify.sh
@@ -1,28 +1,57 @@
 #!/usr/bin/env bash
+# save it as /usr/local/bin/notify_about_attack.sh
+
+# @halbebruno - Via Livre
+
+USERID=" "
+KEY=" "
+TIMEOUT="10"
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+
 
 # This script will get following params:
-#     $1 client address
-#     $2 attack direction
-#     $3 pps
-#     $4 action
+#  $1 client_ip_as_string
+#  $2 data_direction
+#  $3 pps_as_string
+#  $4 action (ban or unban)
 
-details=$(< /dev/stdin)
+#
+# Please be carefult! You should not remove cat >
+#
 
-. /fastnetmon/bin/functions.sh
+if [ "$4" = "unban" ]; then
+            # No details arrived to stdin here
 
-case "$4" in
-    ban)
-        handle_ban $1
-        ;;
-    attack_details)
-        handle_attack_details $1
-        ;;
-    unban)
-        handle_unban $1
-        ;;
-    *)
-        handle_unknown_action $1
-        ;;
-esac
+                # Unban actions if used
+                    exit 0
+            fi
 
-exit $ret_code
+            #
+            # For ban and attack_details actions we will receive attack details to stdin
+            # if option notify_script_pass_details enabled in FastNetMon's configuration file
+            #
+            # If you do not need this details, please set option notify_script_pass_details to "no".
+            #
+            # Please do not remove "cat" command if you have notify_script_pass_details enabled, because
+            # FastNetMon will crash in this case (it expect read of data from script side).
+            #
+
+            if [ "$4" = "ban" ]; then
+                        cat | curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&text=FastNetMon Guard: IP $1 blocked because $2 attack with power $3 pps" $URL > /dev/null ;
+                            # You can add ban code here!
+                                exit 0
+                        fi
+
+                        if [ "$4" == "attack_details" ]; then
+                                    cat | curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&text=FastNetMon Guard: IP $1 blocked because $2 attack with power $3 pps" $URL > /dev/null ;
+
+                                        exit 0
+                                fi
+
+~
+~
+~
+~
+~
+~
+~
