34a4982344289186f7fc9fa8e9007de3dd79a8a6
deivisonmarteleto <42618693+deivisonmarteleto@users.noreply.github.com>
2020-03-09 13:56:31 -0300

    Add files via upload

---
+++ container/fastnetmon/bin/notify.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# This script will get following params:
+#     $1 client address
+#     $2 attack direction
+#     $3 pps
+#     $4 action
+
+details=$(< /dev/stdin)
+
+. /fastnetmon/bin/functions.sh
+
+case "$4" in
+    ban)
+        handle_ban $1
+        ;;
+    attack_details)
+        handle_attack_details $1
+        ;;
+    unban)
+        handle_unban $1
+        ;;
+    *)
+        handle_unknown_action $1
+        ;;
+esac
+
+exit $ret_code
